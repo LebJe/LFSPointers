44c0d08be4ebd33473fcf59c865c1a9e95a79ea1
Jeff L <51171427+LebJe@users.noreply.github.com>
2020-04-07 15:50:10 -0400

    0.0.1

--- Package.swift
+++ Package.swift
@@ -7,14 +7,17 @@ let package = Package(
     name: "LFSPointers",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/kareman/SwiftShell", from: "5.0.1"),
+		 .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.4")),
+		 .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.5")
+		 
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "LFSPointers",
-            dependencies: []),
+            dependencies: ["SwiftShell", .product(name: "ArgumentParser", package: "swift-argument-parser"), "Rainbow"]),
         .testTarget(
             name: "LFSPointersTests",
             dependencies: ["LFSPointers"]),
--- Sources/LFSPointers/main.swift
+++ Sources/LFSPointers/main.swift
@@ -1,1 +1,94 @@
-print("Hello, world!")
+import SwiftShell
+import Foundation
+import ArgumentParser
+import Rainbow
+
+struct LFSPointers: ParsableCommand {
+	static let configuration = CommandConfiguration(
+		abstract: "Replaces all large files with Git LFS pointers.")
+	
+	@Argument(help: "The directory which contains the files you want to convert to LFS pointers.")
+	var directory: String
+	
+	@Argument(help: "The regular expression used to filter files (\"*\" will match everything).")
+	var regularExpression: String
+	
+	@Flag(help: "Whether to display verbose output.")
+	var verbose: Bool
+	
+	func run() throws {
+		let fm = FileManager()
+		
+		do {
+			
+			for file in try fm.contentsOfDirectory(atPath: directory) {
+				
+				let regex = try NSRegularExpression(pattern: regularExpression)
+				
+				if regex.matches(file) {
+					if verbose {
+						print("Converting \"\(file)\" to pointer using \"git lfs pointer --file=\(file)\"...\n")
+					} else {
+						print("Converting \"\(file)\" to pointer...\n")
+					}
+					
+					let r = SwiftShell.run("git", "lfs", "pointer", "--file=\(directory + file)")
+					var result = r.stdout
+					var error = r.stderror
+					
+					guard !result.isEmpty && error != "read \(file): is a directory" else {
+						if verbose {
+							print("Could not convert \"\(file)\" to a pointer.\n Git LFS error: \(error)\n".red)
+						} else {
+							print("Could not convert \"\(file)\" to a pointer.".red)
+						}
+						
+						result = ""
+						error = ""
+						
+						continue
+					}
+					
+					if verbose {
+						print("Removing \"Git LFS pointer for \(file)\" from output of previous command...\n")
+					}
+					
+					result = result.replacingOccurrences(of: "Git LFS pointer for \(file)", with: "")
+					
+					
+					if verbose {
+						print("Removing large file \"\(file)\"...\n")
+					} else {
+						print("Replacing file \"\(file)\" with pointer file...\n")
+					}
+					
+					try fm.removeItem(atPath: file)
+					
+					if verbose {
+						print("Creating file \"\(file)\" with contents \"\(result)\"...\n")
+					}
+					
+					fm.createFile(atPath: file, contents: result.data(using: .utf8)!, attributes: nil)
+				} else {
+					if verbose {
+						print("File name \"\(file)\" does not match regular expression \"\(regularExpression)\", continuing...")
+					}
+				}
+
+			}
+			
+		} catch let error {
+			print("An error occurred: \(error)".red)
+			Foundation.exit(2)
+		}
+	}
+}
+
+LFSPointers.main()
+
+extension NSRegularExpression {
+	func matches(_ string: String) -> Bool {
+		let range = NSRange(location: 0, length: string.utf16.count)
+		return firstMatch(in: string, options: [], range: range) != nil
+	}
+}
