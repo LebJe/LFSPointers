7669a23d9e73056e3b8c394ab925f606b9ad5b86
LebJe <51171427+LebJe@users.noreply.github.com>
2020-10-06 15:26:39 +0000

    Use Docker image when testing on Linux, and remove unnecessary scripts

--- Scripts/script.sh
+++
@@ -1,8 +0,0 @@
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  RELEASE_DOT=$(lsb_release -r)
-  RELEASE_NUM=$(cut -f2 <<< "$RELEASE_DOT")
-  export PATH="${PWD}/swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_NUM}/usr/bin:$PATH"
-fi
-
-swift build
-swift test
