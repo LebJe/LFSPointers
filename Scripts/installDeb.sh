593a7f1b8161678ad2f29c4defaf49eb4fd4562d
LebJe <51171427+LebJe@users.noreply.github.com>
2021-01-30 21:33:12 +0000

    Update version number in installDeb.sh

--- Scripts/installDeb.sh
+++ Scripts/installDeb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 case $(uname -m) in
-	x86_64) wget -O LFSPointers.deb "https://github.com/LebJe/LFSPointers/releases/download/3.0.1/LFSPointers_3.0.1-1_amd64.deb" ;;
-	aarch64) wget -O LFSPointers.deb "https://github.com/LebJe/LFSPointers/releases/download/3.0.1/LFSPointers_3.0.1-1_arm64.deb" ;;
+	x86_64) wget -O LFSPointers.deb "https://github.com/LebJe/LFSPointers/releases/download/3.0.1/LFSPointers_4.0.0-1_amd64.deb" ;;
+	aarch64) wget -O LFSPointers.deb "https://github.com/LebJe/LFSPointers/releases/download/3.0.1/LFSPointers_4.0.0-1_arm64.deb" ;;
 esac
 
 dpkg -i LFSPointers.deb
